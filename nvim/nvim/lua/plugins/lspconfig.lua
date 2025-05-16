2ac7480dc611d5c8db5ac0e1f507b68a62f8faba
Robert Serrano <rserranon@gmail.com>
2025-05-16 11:20:37 -0500

    Configure neovim for python

--- nvim/nvim/lua/plugins/lspconfig.lua
+++ nvim/nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,24 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     setup = {
+      pyright = function(_, opts)
+        opts.settings = vim.tbl_deep_extend("force", opts.settings or {}, {
+          python = {
+            venvPath = vim.fn.stdpath("data") .. "/envs",
+            venv = ".venv",
+            defaultInterpreterPath = ".venv/bin/python",
+            analysis = {
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              reportMissingTypeStubs = false,
+              diagnosticMode = "workspace",
+            },
+          },
+        })
+        require("lspconfig").pyright.setup(opts)
+        return true
+      end,
       rust_analyzer = function()
         return true
       end,
