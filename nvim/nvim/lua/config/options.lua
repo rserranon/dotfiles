fa544520f49b709c12965ae482b3b60a06b20ca6
Robert Serrano <rserranon@gmail.com>
2025-05-19 18:58:33 -0500

    Fix catppuccin theme

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -2,3 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+local opt = vim.opt
+
+opt.number = true -- show line numbers
+opt.relativenumber = true -- relative line numbers
+opt.wrap = false -- no wrap
+opt.updatetime = 300 -- faster CursorHold
+opt.clipboard = "unnamedplus" -- use system clipboard for all yank, delete, paste operations
+
+-- if you prefer the vim.o API, you could instead write:
+-- vim.o.clipboard = "unnamedplus"
--- nvim/nvim/lua/plugins/catppuccin.lua
+++ nvim/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,26 @@
+-- lua/plugins/catppuccin.lua
 return {
-  { "catppuccin/nvim" },
-  lazy = false,
-  name = "catppuccin",
-
   {
-    "LazyVim/LazyVim",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      flavour = "macchiato",
+      background = { light = "latte", dark = "macchiato" },
+      transparent_background = true,
+      term_colors = true,
+      styles = {
+        comments = { "italic" },
+        keywords = { "bold" },
+        functions = {}, -- leave empty if you don’t need special styles
+        strings = {},
+        variables = {},
+      },
     },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
